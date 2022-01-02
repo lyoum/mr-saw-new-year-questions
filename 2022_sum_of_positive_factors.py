f7796ac26a8617ad6cf65e3c24cfaa885b5c8d98
YouMing <lyoum97@gmail.com>
2022-01-02 08:50:08 +0800

    Fix inaccurate calculation
    - Fix inaccurate total year calculation
    - Fix inaccurate sum of year list
    - Revised loop range from 2001 to 2100 instead of 2000 to 2099

--- 2022_sum_of_positive_factors.py
+++ 2022_sum_of_positive_factors.py
@@ -1,5 +1,5 @@
 ## Question:
-## Within this century (2000 - 2099), how many years can have sum of it's positive factors, without repetition?
+## Within this century (2001 - 2100), how many years can have at least 3 sums of it's positive factors, without repetition?
 ## Examples:
 ## 2022 = 1011 + 674 + 337
 ## 2000 = 1000 + 500 + 400 + 100
@@ -11,23 +11,46 @@ accumulateYears = []
 def printFactorialYear(year: int) :
     factorial = 2;
     accumulateSum = 0
-    accumulateList = []
+    actualSumList = []
+    excludeFactorList = []
+    possibleSumList = []
+    possibleFactorToSum = 0
+    global accumulateYears
+
+    ## Get all possible factors to possibleSumList
     while True:
         if year % factorial == 0:
             possibleFactorToSum = int(year / factorial)
-            if accumulateSum + possibleFactorToSum <= year:
-                accumulateSum = int(accumulateSum + possibleFactorToSum)
-                accumulateList.append(possibleFactorToSum)
+            possibleSumList.append(possibleFactorToSum)
         factorial+=1
-        if accumulateSum == year:
-            print(f'year: {year}, sum of: {accumulateList}')
-            global accumulateYears
-            accumulateYears.append(year)
+        if factorial >= year:
             break
-        if factorial == year:
+    possibleSumList.append(1) #Every number has 1 as factorial
+
+    ## Get factors to exclude in possibleSumList
+    totalPossibleSumOfYear = sum(possibleSumList)
+    excludeFactorSum = totalPossibleSumOfYear - year
+    for factor in possibleSumList:
+        if factor <= excludeFactorSum:
+            excludeFactorSum = excludeFactorSum - factor
+            excludeFactorList.append(factor)
+        if excludeFactorSum == 0:
             break
 
-for year in range(2000, 2099):
+    ## Get actual sum factors to actualSumList
+    ## Get actual years to accumulateYears
+    for factor in possibleSumList:
+        if factor in excludeFactorList:
+            continue
+        if accumulateSum + factor <= year:
+            accumulateSum = int(accumulateSum + factor)
+            actualSumList.append(factor)
+        if accumulateSum == year and len(actualSumList) >= 3:
+            print(f'year: {year}, is sum of: {actualSumList}')
+            accumulateYears.append(year)
+            break
+
+for year in range(2001, 2101):
     printFactorialYear(year)
 
 print(f'Total years: {len(accumulateYears)}')
